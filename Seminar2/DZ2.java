610dcc1beecdc4f2ff85548a3f93f84708b09e52
Kv260804 <111348505+Kv260804@users.noreply.github.com>
2022-12-30 19:56:10 +0300

    Add files via upload

--- Seminar2/DZ2.java
+++ Seminar2/DZ2.java
@@ -60,7 +60,21 @@
 // ---------------------------------------------------------------------------------
 
 //101. Symmetric Tree
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
 public class Symetric_Tree_101 {
     public static void main(String[] args) {
